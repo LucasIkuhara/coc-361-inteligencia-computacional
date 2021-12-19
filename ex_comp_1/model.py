8a16796c8f9e721b60b2118e7314117a432cc08d
Lucas Ikuhara <lri2911@gmail.com>
2021-12-19 19:43:03 -0300

    feat: add decision tree inital implementation

--- ex_comp_1/model.py
+++ ex_comp_1/model.py
@@ -1,8 +1,21 @@
 #%%
-# Import dependencies
+# Importando pacotes
 import pandas as pd
+from sklearn import tree
 
 #%%
-# Read data
+# Ler dados
 df = pd.read_csv('../star-dataset/Star3642_balanced.csv')
+print('Sample data:')
 print(df.tail())
+
+# Separando Df de features e resultados 
+training_targets = df['TargetClass']
+training_data = df.drop('TargetClass', axis=1)
+training_data = training_data.drop('SpType', axis=1) #TODO Mapear os tipos para tipo int
+
+# Treinar árvore de decisão
+decision_tree = tree.DecisionTreeClassifier()
+decision_tree.fit(training_data, training_targets)
+
+tree.plot_tree(decision_tree)
