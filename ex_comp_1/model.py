2ac6113c940cf7ee86b44bec082cc3bae5b859e8
Lucas Ikuhara <lri2911@gmail.com>
2021-12-19 19:48:16 -0300

    refactor: fix lint errors

--- ex_comp_1/model.py
+++ ex_comp_1/model.py
@@ -1,9 +1,9 @@
-#%%
+# %%
 # Importando pacotes
 import pandas as pd
 from sklearn import tree
 
-#%%
+# %%
 # Ler dados
 df = pd.read_csv('../star-dataset/Star3642_balanced.csv')
 print('Sample data:')
@@ -12,7 +12,7 @@ print(df.tail())
 # Separando Df de features e resultados 
 training_targets = df['TargetClass']
 training_data = df.drop('TargetClass', axis=1)
-training_data = training_data.drop('SpType', axis=1) #TODO Mapear os tipos para tipo int
+training_data = training_data.drop('SpType', axis=1)  # TODO Mapear os tipos para tipo int
 
 # Treinar árvore de decisão
 decision_tree = tree.DecisionTreeClassifier()
