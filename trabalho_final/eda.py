e0bff9edaca20ca3b4b30f51614ad1bebebca754
Lucas Ikuhara <lri2911@gmail.com>
2022-01-03 22:31:58 -0300

    feat: add basic data treatment

--- trabalho_final/eda.py
+++ trabalho_final/eda.py
@@ -15,6 +15,23 @@ df = pd.read_csv('../marketing_data.csv')
 df.head()
 
 # %%
+# Tratamento básico e estatísticas
+
+print('Estatistícas do Dataset', '='*50)
+print(f'O dataset contém {len(df)} linhas, das quais {len(df[df.duplicated()])} são duplicatas.')
+print(f'Cada uma das linhas tem {len(df.columns)} colunas.')
+
+# Limpando valores NaN, Null ou Blank.
+df = df.dropna()
+print(f'''Após a remoção de linhas faltando dados, ou preenhidas incorretamente (NaN, null..),
+ficamos com {len(df)}  linhas.''')
+
+# Remover coluna ID
+df = df.drop('ID', axis=1)
+print(f'''Além disso, a coluna ID foi removida, pois não agrega a nossa exploração.
+Dessa maneira, ficamos com {len(df.columns)} colunas.''')
+
+# %%
 # Histograma de cada coluna
 
 base = os.getcwd()  # Salva endereço atual
@@ -25,7 +42,13 @@ nao_criados = []  # Salvar referências de falhas
 # Gerar e salvar gráficos
 for column in df.columns:
     try:
-        fig = px.histogram(df, x=column, nbins=10)
+        # Caso a coluna tenha poucas entradas únicas
+        # Usar a quantidade de entradas como quantidade de bins
+        # Caso o contrário, usar 10
+        if bins := len(df[column].unique()) > 10:
+            bins = 10
+
+        fig = px.histogram(df, x=column, nbins=bins)
         fig.show()
         print(f"Gráfico acima: {column}")
         fig.write_image(f"{column}.png")
@@ -51,7 +74,8 @@ plt.yticks(range(df.select_dtypes(['number']).shape[1]), df.select_dtypes(['numb
 cb = plt.colorbar(extend='both')
 plt.clim(-1, 1)
 cb.ax.tick_params(labelsize=14)
-plt.title('Correlation Matrix', fontsize=16, pad=100)
+plt.title('Correlation Matrix', fontsize=16, pad=80)
+plt.savefig('matrix_de_correlacao.png')
 
 # Referência: https://stackoverflow.com/questions/29432629/plot-correlation-matrix-using-pandas
 
