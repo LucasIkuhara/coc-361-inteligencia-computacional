4a34c3c99a89c83aee18dc0da25a9f513dca233d
Lucas Ikuhara <lri2911@gmail.com>
2022-02-03 12:00:33 -0300

    feat: add complete visualization suite

--- ex_comp_1/model.py
+++ ex_comp_1/model.py
@@ -2,7 +2,12 @@
 # Importando pacotes
 import numpy as np
 import pandas as pd
-from sklearn import tree
+from sklearn import tree, ensemble
+import matplotlib.pyplot as plt
+from numpy.lib.arraypad import _pad_dispatcher, pad
+import pandas as pd
+import numpy as np
+import os
 import plotly.express as px
 
 # %%
@@ -14,33 +19,72 @@ print(df.tail())
 # %%
 # Visualização inicial
 
+# Configuração dos gráficos
+config = {
+    'displaylogo': False,
+    'scrollZoom': True
+}
+
+
 # Função de plot
 def plot(subtitulo=False):
 
-    # Configuração dos gráficos
-    config = {
-        'displaylogo': True,
-        'logo': False
-    }
-
+    # Gerar um histograma por coluna
     for coluna in df.columns:
         titulo = coluna
-        if subtitulo: titulo += f': {subtitulo}'
-        vis = px.histogram(df, x=coluna, color='TargetClass', template='plotly_dark', title=titulo)
+        if subtitulo:
+            titulo += f': {subtitulo}'
+
+        vis = px.histogram(df, x=coluna, color='TargetClass',
+                           template='plotly_dark', title=titulo,
+                           marginal="violin")
         vis.show(config=config)
 
 
 plot()
 
 # %%
+# Correlação entre variáveis
+
+fig = px.imshow(df.corr(), template='plotly_dark',
+                text_auto=True, title='Matiz de Correlação',
+                color_continuous_scale=px.colors.sequential.Agsunset,
+                range_color=(-1, 1))
+
+fig.show(config=config)
+
+# %%
 # Limpeza e tratamento de dados
+# Referência: https://towardsdatascience.com/feature-engineering-for-machine-learning-3a5e293a5114
 
-apicar_log = ()
+# Retirar nulos, NaN e NA
+df = df.dropna()
+print(f'''Após a remoção de linhas faltando dados, ou preenhidas incorretamente (NaN, null..),
+ficamos com {len(df)}  linhas.''')
 
-for coluna in apicar_log:
-    df[coluna] = (df[coluna]).transform(np.log)
-fig = px.histogram(df2, x='SpType', y='occ_percetual')
-fig.show()
+# Dropar a coluna SpType (584 categorias diferentes, menos de 1% de ocorrência
+# da classe mais representada)
+df = df.drop('SpType', axis=1)
+
+# Transformações de Log
+colunas_alvo_log = ('Plx', 'e_Plx')
+
+for coluna in colunas_alvo_log:
+    df[coluna] = df[coluna].transform(np.log)
+
+
+# Standartização
+def standardize(coluna):
+    return (coluna - coluna.mean()) / coluna.std()
+
+
+colunas_alvo_std = ('Vmag', 'Plx', 'e_Plx', 'B-V', 'Amag')
+for coluna in colunas_alvo_std:
+    df[coluna] = (df[coluna]).transform(standardize)
+
+plot('ajustado')
+
+# %%
 # print(df2.tail())
 
 # #%%
@@ -48,6 +92,13 @@ fig.show()
 # training_targets = df['TargetClass']
 # training_data = df.drop('TargetClass', axis=1)
 # training_data = training_data.drop('SpType', axis=1)  # TODO Mapear os tipos para tipo int
+# Tratamento básico
+
+
+# %%
+# Separando Df de features e resultados
+training_targets = df['TargetClass']
+training_data = df.drop('TargetClass', axis=1)
 
 # # Treinar árvore de decisão
 # decision_tree = tree.DecisionTreeClassifier()
@@ -57,3 +108,7 @@ fig.show()
 # tree.plot_tree(decision_tree)
 
 # %%
+# Treinar
+grad_boosting = ensemble.GradientBoostingClassifier()
+
+# tree.plot_tree(decision_tree)
