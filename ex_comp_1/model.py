a640d478c4459040ed3006f70f5a20a4df89a6b9
Lucas Ikuhara <lri2911@gmail.com>
2022-02-04 17:56:05 -0300

    feat: add cross validated training for dec. trees

--- ex_comp_1/model.py
+++ ex_comp_1/model.py
@@ -3,11 +3,10 @@
 import numpy as np
 import pandas as pd
 from sklearn import tree, ensemble
-import matplotlib.pyplot as plt
-from numpy.lib.arraypad import _pad_dispatcher, pad
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 import pandas as pd
 import numpy as np
-import os
 import plotly.express as px
 
 # %%
@@ -70,7 +69,7 @@ df = df.drop('SpType', axis=1)
 colunas_alvo_log = ('Plx', 'e_Plx')
 
 for coluna in colunas_alvo_log:
-    df[coluna] = df[coluna].transform(np.log)
+    df[coluna] = (df[coluna] + 1).transform(np.log)
 
 
 # Standartização
@@ -82,30 +81,48 @@ colunas_alvo_std = ('Vmag', 'Plx', 'e_Plx', 'B-V', 'Amag')
 for coluna in colunas_alvo_std:
     df[coluna] = (df[coluna]).transform(standardize)
 
+# Retirar nulos, NaN e NA que podem ser gerados durante os ajustes
+df = df.dropna()
+
 plot('ajustado')
 
 # %%
-# print(df2.tail())
-
-# #%%
-# # Separando Df de features e resultados
-# training_targets = df['TargetClass']
-# training_data = df.drop('TargetClass', axis=1)
-# training_data = training_data.drop('SpType', axis=1)  # TODO Mapear os tipos para tipo int
-# Tratamento básico
-
-
-# %%
 # Separando Df de features e resultados
+
 training_targets = df['TargetClass']
 training_data = df.drop('TargetClass', axis=1)
 
-# # Treinar árvore de decisão
-# decision_tree = tree.DecisionTreeClassifier()
-# decision_tree.fit(training_data, training_targets)a
+# %%
+# Treinamento com validação cruzada (K-Fold CV)
 
 
-# tree.plot_tree(decision_tree)
+# Avaliação de modelo com validação cruzada (k=10 por default)
+def k_fold_cv(model, k=10):
+    score = cross_val_score(model, training_data, training_targets, cv=k)
+    print(f'Acuracia: {score.mean():.3f}, Desvio padrão: {score.std():.3f}', end='\n\n')
+
+    return (score.mean(), score.std())
+
+
+# Testando diferentes modelos
+print('Testando árvores de decisão:')
+
+print('Critério "gini", profundidade máxima 10')
+model = tree.DecisionTreeClassifier(criterion='gini', max_depth=10)
+k_fold_cv(model)
+
+print('Critério "gini", profundidade máxima ilimitada')
+model = tree.DecisionTreeClassifier(criterion='gini', max_depth=None)
+k_fold_cv(model)
+
+print('Critério "entropy", profundidade máxima 10')
+model = tree.DecisionTreeClassifier(criterion='entropy', max_depth=10)
+k_fold_cv(model)
+
+print('Critério "entropy", profundidade máxima 10')
+model = tree.DecisionTreeClassifier(criterion='entropy', max_depth=None)
+k_fold_cv(model)
+
 
 # %%
 # Treinar
